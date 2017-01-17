1e235547c8b6e6bf764d59d0fd63857565c59711
Umar <umar432@gmail.com>
2017-01-17 12:03:57 +0100

    Udated - W3 School CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  w3_open(){
+    document.getElementById("mySidenav").style.display = "block";
+  }
+  w3_close(){
+    document.getElementById("mySidenav").style.display = "none";
+  }
 }
